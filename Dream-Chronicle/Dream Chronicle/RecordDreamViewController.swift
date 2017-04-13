d8cd6a30199dda2104358d21c6b581021441feee
Austin Luk <al2gq@labmini07.lab.cs.virginia.edu>
2017-04-13 19:23:59 -0400

    Please work

---
+++ Dream-Chronicle/Dream Chronicle/RecordDreamViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RecordDreamViewController.swift
+//  Dream Chronicle
+//
+//  Created by Austin Luk on 4/13/17.
+//  Copyright © 2017 Austin Luk. All rights reserved.
+//
+
+import UIKit
+
+class RecordDreamViewController: UIViewController {
+
+    @IBOutlet weak var tagTextField: UITextField!
+    @IBOutlet weak var dreamTextField: UITextField!
+    
+    @IBAction func saveData(_ sender: UIButton) {
+        print("test")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    
+    
+}
---
+++ Dream-Chronicle/Dream ChronicleTests/Dream_ChronicleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dream_ChronicleTests.swift
+//  Dream ChronicleTests
+//
+//  Created by Austin Luk on 4/8/17.
+//  Copyright © 2017 Austin Luk. All rights reserved.
+//
+
+import XCTest
+@testable import Dream_Chronicle
+
+class Dream_ChronicleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
